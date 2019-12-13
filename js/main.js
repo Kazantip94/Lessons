af6f8f0e89fde8a8c15008a2b1445567423cf92c
Kazantip94 <56036660+Kazantip94@users.noreply.github.com>
2019-12-14 00:59:28 +0200

    feat: add closing the form with esc and clicking outside the form in main.js

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function(event){
     const modal = document.querySelector('.modal');
     const modalBtn = document.querySelectorAll('[data-toggle=modal]');
     const closeBtn = document.querySelector('.modal__close');
+    const modalDialog = document.querySelector('.modal-dialog');
     const switchModal = () => {
         modal.classList.toggle('modal--visible');
     }
@@ -11,4 +12,20 @@ document.addEventListener("DOMContentLoaded", function(event){
     }); 
 
     closeBtn.addEventListener('click', switchModal);
+
+    window.addEventListener('click', function(event) {
+        if (event.target == modal) {
+          modal.classList.toggle('modal--visible');
+          modalDialog.classList.toggle('modal-dialog--visible');
+          document.body.style.overflow = "auto";
+        }
+      }); 
+
+    window.onkeyup = function (event) {
+        if (event.keyCode == 27) {
+            modal.classList.toggle('modal--visible');
+            modalDialog.classList.toggle('modal-dialog--visible');
+            document.body.style.overflow = "auto";
+        }
+       }   
 });
