352a5d280b3fcd277322cad74dc1be4beca66d21
Kazantip94 <56036660+Kazantip94@users.noreply.github.com>
2019-12-16 16:23:56 +0200

    fix: background-img in modal__dialog

--- js/main.js
+++ js/main.js
@@ -1,59 +1,57 @@
-document.addEventListener("DOMContentLoaded", function(event){
-    const modal = document.querySelector('.modal');
-    const modalBtn = document.querySelectorAll('[data-toggle=modal]');
-    const closeBtn = document.querySelector('.modal__close');
+// document.addEventListener("DOMContentLoaded", function(event){
+//     const modal = document.querySelector('.modal');
+//     const modalBtn = document.querySelectorAll('[data-toggle=modal]');
+//     const closeBtn = document.querySelector('.modal__close');
     
   
-    modalBtn.forEach(element => {
-        element.addEventListener('click', () => {
-            modal.classList.toggle('modal--visible');
-        });
-    }); 
+//     modalBtn.forEach(element => {
+//         element.addEventListener('click', () => {
+//             modal.classList.toggle('modal--visible');
+//         });
+//     }); 
    
-    closeBtn.addEventListener('click', function() {
-        modal.classList.toggle('modal--visible');
+//     closeBtn.addEventListener('click', function() {
+//         modal.classList.toggle('modal--visible');
+//     });
+
+//     window.addEventListener('click', function(event) {
+//         if (event.target == modal) {
+//           modal.classList.toggle('modal--visible');
+//         }
+//       });
+    
+//     document.addEventListener('keydown', function(event) {
+//         if (event.keyCode == 27) {
+//             modal.classList.toggle('modal--visible', false);
+            
+//         }
+//       });
+   
+// });
+
+$(document).ready(function() {	
+    var modal = $('.modal'),
+        modalBtn = $('[data-toggle="modal"]'),
+        closeBtn = $('.modal__close');
+
+    modalBtn.on('click', function() {
+        modal.toggleClass('modal--visible'); 
     });
 
-    window.addEventListener('click', function(event) {
-        if (event.target == modal) {
-          modal.classList.toggle('modal--visible');
-        }
-      });
+    closeBtn.on('click', function() {
+        modal.toggleClass('modal--visible'); 
+    });
+
+    $(document).keydown(function(e) {
+		if (e.keyCode === 27) {
+            modal.toggleClass('modal--visible', false);  
+		}
+	});
     
-    document.addEventListener('keydown', function(event) {
-        if (event.keyCode == 27) {
-            modal.classList.toggle('modal--visible', false);
-            
+    $('.modal').click(function(e) {
+		if ($(e.target).closest('.modal__dialog').length == 0) {
+			$(this).toggleClass('modal--visible');		
         }
-      });
-   
+    });
 });
 
-// $(document).ready(function(){	
-//     var modal = $('.modal'),
-//         modalBtn = $('[data-toggle="modal"]'),
-//         closeBtn = $('.modal__close');
-//         modalDialog = $('.modal__dialog')
-
-//     modalBtn.on('click', function(){
-//         modal.show(); 
-//         modalDialog.show();
-//     });
-
-//     closeBtn.on('click', function(){
-//         modal.hide();
-//         modalDialog.hide();
-//     });
-//     $(document).keydown(function(e) {
-// 		if (e.keyCode === 27) {
-//             modal.hide();  
-//             modalDialog.hide();
-// 		}
-// 	});
-    
-//     $('.modal').click(function(e) {
-// 		if ($(e.target).closest('.modal__dialog').length == 0) {
-// 			$(this).hide();		
-//         }
-//     });
-// });
