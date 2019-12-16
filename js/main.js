ef1c148c7e5321bfb2e709f4ae4487deaf31346b
Kazantip94 <56036660+Kazantip94@users.noreply.github.com>
2019-12-16 16:09:11 +0200

    fix: double tap on esc

--- js/main.js
+++ js/main.js
@@ -2,30 +2,58 @@ document.addEventListener("DOMContentLoaded", function(event){
     const modal = document.querySelector('.modal');
     const modalBtn = document.querySelectorAll('[data-toggle=modal]');
     const closeBtn = document.querySelector('.modal__close');
-    const modalDialog = document.querySelector('.modal-dialog');
-    const switchModal = () => {
-        modal.classList.toggle('modal--visible');
-    }
-
-    modalBtn.forEach(element =>{
-        element.addEventListener('click', switchModal);
+    
+  
+    modalBtn.forEach(element => {
+        element.addEventListener('click', () => {
+            modal.classList.toggle('modal--visible');
+        });
     }); 
-
-    closeBtn.addEventListener('click', switchModal);
+   
+    closeBtn.addEventListener('click', function() {
+        modal.classList.toggle('modal--visible');
+    });
 
     window.addEventListener('click', function(event) {
         if (event.target == modal) {
           modal.classList.toggle('modal--visible');
-          modalDialog.classList.toggle('modal-dialog--visible');
-          document.body.style.overflow = "auto";
         }
-      }); 
-
-    window.onkeyup = function (event) {
+      });
+    
+    document.addEventListener('keydown', function(event) {
         if (event.keyCode == 27) {
-            modal.classList.toggle('modal--visible');
-            modalDialog.classList.toggle('modal-dialog--visible');
-            document.body.style.overflow = "auto";
+            modal.classList.toggle('modal--visible', false);
+            
         }
-       }   
+      });
+   
 });
+
+// $(document).ready(function(){	
+//     var modal = $('.modal'),
+//         modalBtn = $('[data-toggle="modal"]'),
+//         closeBtn = $('.modal__close');
+//         modalDialog = $('.modal__dialog')
+
+//     modalBtn.on('click', function(){
+//         modal.show(); 
+//         modalDialog.show();
+//     });
+
+//     closeBtn.on('click', function(){
+//         modal.hide();
+//         modalDialog.hide();
+//     });
+//     $(document).keydown(function(e) {
+// 		if (e.keyCode === 27) {
+//             modal.hide();  
+//             modalDialog.hide();
+// 		}
+// 	});
+    
+//     $('.modal').click(function(e) {
+// 		if ($(e.target).closest('.modal__dialog').length == 0) {
+// 			$(this).hide();		
+//         }
+//     });
+// });
